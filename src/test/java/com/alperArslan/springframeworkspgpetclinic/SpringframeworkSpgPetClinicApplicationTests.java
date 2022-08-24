6a809a0c4c3b89ce4e60510790c6784bb6b3bfe4
Alper.Arslan <alper.arslan663@gmail.com>
2022-08-24 09:37:46 +0300

    Initial Project Add

---
+++ src/test/java/com/alperArslan/springframeworkspgpetclinic/SpringframeworkSpgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alperArslan.springframeworkspgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringframeworkSpgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
