346460d1fe203b46a414bce19a24958b078d8133
Alper.Arslan <alper.arslan663@gmail.com>
2022-08-26 17:11:07 +0300

    Added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/com/alperArslan/springframeworkspgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.alperArslan.springframeworkspgpetclinic.services;
+
+import com.alperArslan.springframeworkspgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long>{
+}
