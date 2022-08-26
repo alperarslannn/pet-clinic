c11e2e27fd6d2c5ff1124898467c1b1ca3435e9e
Alper.Arslan <alper.arslan663@gmail.com>
2022-08-26 14:57:48 +0300

    Added Visit, updated Owner with Pet relationship. Closes #26

--- pet-clinic-data/src/main/java/com/alperArslan/springframeworkspgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/alperArslan/springframeworkspgpetclinic/model/Owner.java
@@ -1,4 +1,14 @@
 package com.alperArslan.springframeworkspgpetclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
 public class Owner extends Person{
+    private Set<Pet> pets;
+
+
 }
