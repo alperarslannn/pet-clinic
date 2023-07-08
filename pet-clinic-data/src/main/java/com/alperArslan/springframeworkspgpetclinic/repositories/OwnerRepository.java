2add0c0044e8eac110d75a35a4fa7846ed59e640
alperarslannn <alper.arslan663@gmail.com>
2023-07-08 16:32:40 +0300

    implemented find owner with InitBinder. Closes #54

--- pet-clinic-data/src/main/java/com/alperArslan/springframeworkspgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/alperArslan/springframeworkspgpetclinic/repositories/OwnerRepository.java
@@ -3,6 +3,10 @@ package com.alperArslan.springframeworkspgpetclinic.repositories;
 import com.alperArslan.springframeworkspgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner,Long> {
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLikeIgnoreCase(String lastName);
 }
--- pet-clinic-data/src/main/java/com/alperArslan/springframeworkspgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/alperArslan/springframeworkspgpetclinic/services/OwnerService.java
@@ -2,7 +2,10 @@ package com.alperArslan.springframeworkspgpetclinic.services;
 
 import com.alperArslan.springframeworkspgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long>{
     Owner findByLastName(String lastName);
 
+    List<Owner> findAllByLastNameLike(String lastName);
 }
