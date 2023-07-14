085afe0a9074dfb5357f41928587ad1c1b2b4bc6
alperarslannn <alper.arslan663@gmail.com>
2023-07-14 13:45:27 +0300

    added vets json endpoint. closes #65
    (cherry picked from commit d27bf9e38ca52025bd0c28e23614246d5d46cd20)

--- pet-clinic-web/src/main/java/com/alperarslan/springframeworkspgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/alperarslan/springframeworkspgpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.alperarslan.springframeworkspgpetclinic.controllers;
 
+import com.alperarslan.springframeworkspgpetclinic.model.Vet;
 import com.alperarslan.springframeworkspgpetclinic.services.VetService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -13,11 +18,16 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets","/vets/index","/vets/index.html","/vets.html"})
+    @GetMapping({"/vets","/vets/index","/vets/index.html","/vets.html"})
     public String listVets(Model model){
 
         model.addAttribute("vets",vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public ResponseEntity<Set<Vet>> getVetsJson(){
+        return ResponseEntity.ok(vetService.findAll());
     }
 }
